e9b92658ff8b6cfb360f22ee2bac6a7a623c6d22
Neil Johnson <njohn@thingsforhackers.com>
2016-04-13 21:45:40 +0100

    Controll of LEDs & tone

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# Arduino-Make
+# Copy Me
 
 Author:  njohn@thingsforhackers.com
 
-License: GPL
+License: MIT
 
 More details can be found at the thingsforhackers [blog](https://thingsforhackers.com/blog/)
 
-Simple Makefile based approach to building Arduino compatible programs.
+A simple memory game, similar to "Simon Says"
--- copyme.cpp
+++ copyme.cpp
@@ -1,19 +1,70 @@
 #include <Arduino.h>
-// the setup function runs once when you press reset or power the board
+
+
+//Our LEDS
+static const int RED_LED_PIN = 9;
+static const int BLUE_LED_PIN = 10;
+static const int YELLOW_LED_PIN = 11;
+static const int GREEN_LED_PIN = 6;
+
+//Our Buttons
+static const int RED_BUTTON_PIN = 7;
+static const int BLUE_BUTTON_PIN = 8;
+static const int YELLOW_BUTTON_PIN = 12;
+static const int GREEN_BUTTON_PIN = 13;
+
+static const int SPEAKER_PIN = 3;
+
+static const int NOTE_G4 = 392; //Blue
+static const int NOTE_C4 = 277; //Yellow
+static const int NOTE_A4 = 440; //Red
+static const int NOTE_G5 = 784; //Green
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(13, OUTPUT);
+
+  //Setup LED pins as OUTPUT
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(BLUE_LED_PIN, OUTPUT);
+  pinMode(YELLOW_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+
+  //Setup buttons pins as INPUT_PULLUP
+  pinMode(RED_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(BLUE_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(YELLOW_BUTTON_PIN, INPUT_PULLUP);
+  pinMode(GREEN_BUTTON_PIN, INPUT_PULLUP);
 
   Serial.begin(9600);  //turn on serial communication
-
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  Serial.println("And ON");
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(5000);              // wait for a second
-  Serial.println("And OFF");
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+  // Serial.println("And ON");
+  // digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  // delay(5000);              // wait for a second
+  // Serial.println("And OFF");
+  // digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  // delay(1000);              // wait for a second
+
+  digitalWrite(RED_LED_PIN, !digitalRead(RED_BUTTON_PIN));
+  digitalWrite(BLUE_LED_PIN, !digitalRead(BLUE_BUTTON_PIN));
+  digitalWrite(YELLOW_LED_PIN, !digitalRead(YELLOW_BUTTON_PIN));
+  digitalWrite(GREEN_LED_PIN, !digitalRead(GREEN_BUTTON_PIN));
+
+  if(!digitalRead(RED_BUTTON_PIN))
+  {
+      tone(SPEAKER_PIN, NOTE_A4, 100);
+  }
+  else if(!digitalRead(BLUE_BUTTON_PIN))
+  {
+      tone(SPEAKER_PIN, NOTE_G4, 100);
+  }
+  else if(!digitalRead(YELLOW_BUTTON_PIN))
+  {
+      tone(SPEAKER_PIN, NOTE_C4, 100);
+  }
+  else if(!digitalRead(GREEN_BUTTON_PIN))
+  {
+      tone(SPEAKER_PIN, NOTE_G5, 100);
+  }
+
 }
